00d4c3ed5250756267164734018518c2706f9932
kr-parth07 <jhaparth152@gmail.com>
2025-10-13 22:42:55 +0530

    Add files via upload

---
+++ Day026_to_030/que54.c
@@ -0,0 +1,45 @@
+//Q54: Write a program to print the following pattern:
+/*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include<stdio.h>
+
+int main(){
+    int i, j, space;
+
+    for (i = 1; i <= 5; i++) {
+        for (space = 1; space <= 5 - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i = 4; i >= 1; i--) {
+        for (space = 1; space <= 5 - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day026_to_030/que57.c
@@ -0,0 +1,37 @@
+//Q57: Find the sum of array elements.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+2 4 6 8
+Output 1:
+20
+
+Input 2:
+3
+1 1 1
+Output 2:
+3
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array of size n
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i]; // Add each element to sum
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
